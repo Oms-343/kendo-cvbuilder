d6cdc3ec8143dd103f96e10afdfdc3304b1a84c7
Oms-343 <oms87473@gmail.com>
2025-09-25 11:42:56 +0530

    setting up react and kendo react ui

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@progress/kendo-react-buttons"
+
+
+function App() {
+  return (
+    <div>
+      <h1>hello there</h1>
+      <Button>My KendoReact Button</Button>
+    </div>
+  )
+}
+
+export default App
