668d150363d601c43622eecf6d605da5fc857f8b
Oms-343 <oms87473@gmail.com>
2025-09-26 16:22:20 +0530

    minor ui update for sidebar navigation

--- src/components/LeftPanel/SidebarNavigation.tsx
+++ src/components/LeftPanel/SidebarNavigation.tsx
@@ -33,7 +33,7 @@ const SidebarNavigation: React.FC<SidebarNavigationProps> = ({
         transition: "width 300ms ease-in-out",
         display: "flex",
         flexDirection: "column",
-        padding: "20px 0",
+        padding: "20px 10px",
         alignItems: "center",
         gap: "8px",
       }}
