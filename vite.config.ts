6526d9e1fdb38611ae516cb47379c4d4f20450db
Oms-343 <oms87473@gmail.com>
2025-09-29 12:26:50 +0530

    deployment fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          kendo: [
+            "@progress/kendo-react-buttons",
+            "@progress/kendo-react-inputs",
+            "@progress/kendo-react-labels",
+          ],
+        },
+      },
+    },
+  },
+});
