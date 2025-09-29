59080dfb0fa55b5202c874c53d6bf856d57dccec
Oms-343 <oms87473@gmail.com>
2025-09-29 09:21:32 +0530

    install bootstrap theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "@progress/kendo-theme-default/dist/all.css";
+import "@progress/kendo-theme-bootstrap/scss/all.scss";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
